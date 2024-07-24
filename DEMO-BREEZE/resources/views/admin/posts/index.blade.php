e0b442f3a43279e7e4ac4aa0dd8fffacf54bd9be
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-07-24 17:33:36 +0200

    [UPD] UPDATE INDEX.BLADE

--- DEMO-BREEZE/resources/views/admin/posts/index.blade.php
+++ DEMO-BREEZE/resources/views/admin/posts/index.blade.php
@@ -5,11 +5,11 @@
         <div class="row">
             <div class="col-12 d-flex justify-content-between align-items-center p-3">
                 <h1>
-                    Lista dei lavori
+                    Lista dei Progetti
                 </h1>
                 <div>
                     <a class="btn btn-primary" as="button" href="{{ route('admin.posts.create') }}">
-                        Crea
+                        Crea Progetto
                     </a>
                     {{-- <button class="btn btn-primary">
                         Crea Nuovo
@@ -22,62 +22,48 @@
                     {{ session('message') }}
                 </div>
             @endif
-            <div class="col-12 p-3">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col" class="col">#</th>
-                            <th scope="col" class="col-7">Title</th>
-                            <th scope="col" class="col">slug</th>
-                            <th scope="col" class="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($posts as $post)
-                            <tr>
-                                <th scope="row">
-                                    {{ $post->id }}
-                                </th>
-                                <td>
-                                    @if ($post->cover_image)
-                                        image -
-                                    @endif
-                                    {{ $post->title }}
-                                </td>
-                                <td>
-                                    {{ $post->slug }}
-                                </td>
-                                <td>
-                                    <div class="d-flex gap-2 ">
-                                        <a href="{{ route('admin.posts.show', $post) }}" as="button"
-                                            class="btn btn-info ">
-                                            {{-- <i class="fa-solid fa-magnifying-glass"></i> --}}
-                                            <i class="fa-solid fa-magnifying-glass fa-sm"></i>
-                                        </a>
-                                        <a href="{{ route('admin.posts.edit', $post) }}" as="button"
-                                            class="btn btn-warning ">
-                                            <i class="fa-solid fa-file-pen fa-sm"></i>
-                                        </a>
-                                        <form action="{{ route('admin.posts.destroy', $post) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn btn-danger "><i class="fa-solid fa-trash fa-sm"></i></button>
+            @foreach ($posts as $post)
+                <div class="col-4 g-3 ">
+                    <div class="card p-3" style="width: 25rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Progetto N {{ $post->id }} </h5>
 
-                                        </form>
-                                        {{-- <a href="{{ route('admin.posts.destroy', $post) }}" as="button"
-                                            class="btn btn-danger ">
+                            <p class="card-text">
+                                <strong>
+                                    Titolo:
+                                </strong>
+                                {{ $post->title }}
+                            </p>
+                            <p class="card-text">
+                                <strong>
+                                    Slug:
+                                </strong>
+                                {{ $post->slug }}
+                            </p>
+                            <div class="d-flex gap-2 ">
+                                <a href="{{ route('admin.posts.show', $post) }}" as="button" class="btn btn-info ">
 
-                                        </a> --}}
-                                    </div>
-                                    <div></div>
-                                    <div></div>
-                                </td>
-                            </tr>
-                        @endforeach
+                                    Visualizza
+                                </a>
+                                <a href="{{ route('admin.posts.edit', $post) }}" as="button" class="btn btn-warning ">
+                                    Modifica
+                                </a>
+                                <form action="{{ route('admin.posts.destroy', $post) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger text-black">
+                                        Elimina
+                                    </button>
 
-                    </tbody>
-                </table>
-            </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            @endforeach
+
         </div>
     </div>
 @endsection
